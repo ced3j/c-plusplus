c2a4c7f7c0ad269deb0eb2ced38ce542e30d1888
ced3j <spoonnoisthere@gmail.com>
2024-07-06 08:47:13 +0300

    simple example

--- 01/02.cpp
+++ 01/02.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
 using namespace std;
-int main(){
 
-    cout << "Hello World"; // std:cout << "Hello World";
+int main() {
+    cout << "Hello World" << endl; // std::cout << "Hello World";
+    // Buradaki endl --> endline anlamına gelir, satırı sonlandırıp yeni satıra geçmemizi sağlar
+
+    char letter;
+    cout << "Enter a letter: ";
+    cin >> letter;
+
+    switch(letter) {
+        case 'a':
+        case 'e':
+        case 'u':
+        case 'o':
+        case 'i': 
+            cout << letter << " is a vowel\n"; 
+            break;
+        default: 
+            if (letter >= '0' && letter <= '9') {
+                cout << letter << " is a number, not a letter\n";
+            } else {
+                cout << letter << " is a consonant\n";
+            }
+            break;
+    }
+
     return 0;
 }
