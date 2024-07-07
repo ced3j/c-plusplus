4e1418f65b376917bead28bf3475225fc3b0e41e
ced3j <spoonnoisthere@gmail.com>
2024-07-07 08:30:31 +0300

    data types

---
+++ 01/00.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main(){
+    
+    /*
+    int: Standard integer type, typically 32 bits. ( 4 byte )
+    short: Short integer, usually 16 bits. ( 2 byte )
+    long: Long integer, often 32 or 64 bits. 
+    long long: Very long integer, at least 64 bits
+
+    */
+    int age = 10;
+    short distance = 5;
+    long largeNum = 1000000;
+    long long veryLargeNum = 193294328539493;
+
+
+    /*
+
+    float: Single-precision floating-point, typically 32 bits. ( 4 byte )
+    double: Double-precision floating-point, usually 64 bits.
+    long double: Extended precision floating-point
+
+    */
+   float num1 = 3.14f;
+   double num2 = 2.4532532;
+   long double num3 = 0.32435325932493;
+   
+   
+   
+   // char: Represents a single character
+
+    char grade = 'A';
+
+   // bool: Represents true or false.
+
+    bool isTrue = true;
+    bool isFalse = false;
+
+
+    return 0;
+}
+
+
+
