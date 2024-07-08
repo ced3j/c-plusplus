0d1987bb887929249b3b761382cf08db04ebd74d
ced3j <spoonnoisthere@gmail.com>
2024-07-08 23:56:34 +0300

    cout examples

---
+++ 01/03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "line1" << endl << "line2" << endl;
+
+    short int x = 20;
+    short int y = 45;
+
+    cout << "x: " << x << endl;
+    cout << "x: " << x << " y: " << y << endl;
+    cout << "His father is " << y << " years old" << endl;
+
+    return 0;
+}
