ccf4f100fe7d443aeb3196fabf1ca0a0d75d9f9d
ced3j <spoonnoisthere@gmail.com>
2024-07-09 00:00:55 +0300

    variable fix

--- 01/04.cpp
+++ 01/04.cpp
@@ -11,7 +11,15 @@ int main(){
     cin >> x;
     cout << "You entered the integer: " << x << endl;
 
-    
+    double height = 0;
+    double width = 0;
+    double area = 0;
+
+    cout << "Enter height and width (separated by space): ";
+    cin >> height >> width;
+    area = height * width;
+    cout << height << " x " << width << " = " << area << endl;
+
 
     return 0;
 }
