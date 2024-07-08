676be622f0fcbaae5030900e7bb104303e6f261c
ced3j <spoonnoisthere@gmail.com>
2024-07-08 23:59:36 +0300

    cin examples

--- 01/04.cpp
+++ 01/04.cpp
@@ -1,6 +1,17 @@
+// cin examples
+
 #include <iostream>
 
+using namespace std;
+
 int main(){
+
+    int x;
+    cout << "Enter an integer";
+    cin >> x;
+    cout << "You entered the integer: " << x << endl;
+
     
+
     return 0;
 }
