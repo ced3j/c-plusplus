de93d02de901f6c9bb0576a2efc1d3f139655edb
ced3j <spoonnoisthere@gmail.com>
2024-07-10 12:17:06 +0300

    add project

--- projects/03/main.cpp
+++ projects/03/main.cpp
@@ -2,9 +2,73 @@
 
 using namespace std;
 
-int main(){
+const int MAX_NUMBERS = 10;
 
-    
+void addNumber(int numbers[], int &numCount, int number){
+    if(numCount < MAX_NUMBERS){
+        numbers[numCount++] = number;
+    }else{
+        cout << "Array is full!" << endl;
+    }
+}
+
+
+
+int calculateSum(const int numbers[], int numCount){
+    int sum = 0;
+    for(int i = 0; i < numCount; i++){
+        sum += numbers[i];
+    }
+
+    return sum;
+}
+
+
+void displayNumbers(const int numbers[], int numCount){
+    if(numCount == 0){
+        cout << "No numbers to display" << endl;
+        return;
+    }
+    cout << "Numbers: ";
+    for(int i = 0; i < numCount; ++i){
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    int numbers[MAX_NUMBERS];
+    int numCount = 0;
+    int choice;
+
+    do{
+        cout << "Menu:\n";
+        cout << "1. Add a number\n";
+        cout << "2. Calculate sum of numbers\n";
+        cout << "3. Display all numbers\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if(choice == 1){
+            int number;
+            cout << "Enter number: ";
+            cin >> number;
+            addNumber(numbers, numCount, number);
+        }else if(choice == 2){
+            int sum = calculateSum(numbers, numCount);
+            cout << "Sum of numbers: " << sum << endl;
+        }else if(choice == 3){
+            displayNumbers(numbers, numCount);
+        }else{
+            if(choice == 4){
+                cout << "Exiting...";
+            }else{
+                cout << "Invalid choice. Please try again :)" << endl;
+            }
+        }
+    }while(choice != 4);
 
     return 0;
 }
